9f9f4619a53b88ea8cb2156b0d0acdd3c62296b3
Doaa alhaji <fgdoaa5@gmail.com>
2025-08-25 18:40:43 +0300

    Add Procfile for Railway deploymen1t

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return 'Hello World from Laravel! doaa';
 });
 
 require __DIR__.'/auth.php';
