260cbb8d301b6ab03c65e880e1224365913b6410
Doaa alhaji <fgdoaa5@gmail.com>
2025-08-26 12:59:27 +0300

    تعديلات جديدة على المشروع

--- config/cors.php
+++ config/cors.php
@@ -2,30 +2,20 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    */
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'], // تأكدي أن الـ API و CSRF مسموح لهم
+    'allowed_methods' => ['*'],
 
-    'allowed_methods' => ['*'], // السماح بكل الطرق: GET, POST, PUT, DELETE, إلخ
-
-    'allowed_origins' => [
-        'http://localhost:8001', // التطوير المحلي
-        'https://drone-production-59a0.up.railway.app', // رابط Railway
-        'http://drone-production-59a0.up.railway.app', // رابط Railway
-    ],
+    'allowed_origins' => ['*'],   // هون السماح لكل الأجهزة بما فيها الموبايل
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'], // السماح بكل الهيدرز
+    'allowed_headers' => ['*'],
 
-    'exposed_headers' => ['Authorization', 'Content-Type'], // الهيدرز التي يمكن للـ frontend قراءتها
+    'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => true, // دعم الكوكيز والجلسات
+    'supports_credentials' => false,
 
 ];
